1cdedce69defcf8506f69822c6d2ffada256e9d6
Attila Jakab <attila.jakab@globallogic.com>
2017-11-27 17:41:15 +0100

    Add tests for GET /todos

--- server/server.js
+++ server/server.js
@@ -15,13 +15,13 @@ app.post('/todos', (req, res) => {
 
     todo.save().then((doc) => {
         res.send(doc);
-    }, err => res.status(400).send(e));
+    }, err => res.status(400).send(err));
 });
 
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send(todos);
-    }, err => res.status(400).send(e));
+    }, err => res.status(400).send(err));
 });
 
 app.listen(port, () => {
