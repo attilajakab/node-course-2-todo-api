5e88eb1a02df2bd25217cf69dd5f3f88f72f73fa
Attila Jakab <attila.jakab@globallogic.com>
2017-11-27 17:27:50 +0100

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -15,13 +15,13 @@ app.post('/todos', (req, res) => {
 
     todo.save().then((doc) => {
         res.send(doc);
-    }, (e) => {
-        res.status(400).send(e);
-    });
+    }, err => res.status(400).send(e));
 });
 
 app.get('/todos', (req, res) => {
-    
+    Todo.find().then((todos) => {
+        res.send(todos);
+    }, err => res.status(400).send(e));
 });
 
 app.listen(port, () => {
