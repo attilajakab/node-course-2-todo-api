8e090706ca4ff76286fc9264b7263e7691a3bf3c
Attila Jakab <attila.jakab@globallogic.com>
2017-11-28 10:06:28 +0100

    Add POST /users route and token method

--- server/server.js
+++ server/server.js
@@ -89,6 +89,20 @@ app.patch('/todos/:id', (req, res) => {
     });
 });
 
+app.post('/users', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+    const user = new User(body);
+
+    user.save().then(() => {
+        return user.generateAuthToken();
+    }).then(token => {
+        res.header('x-auth', token).send(user);
+    }).catch(err => {
+        console.log(err);
+        res.status(400).send(err);
+    });
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
 });
