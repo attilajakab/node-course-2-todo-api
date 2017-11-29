06e263916e57d25b1721e3551ff9a9424323ad1c
Attila Jakab <attila.jakab@globallogic.com>
2017-11-29 21:59:05 +0100

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -109,6 +109,18 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+app.post('/users/login', (req, res) => {
+    const body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then(user => {
+        return user.generateAuthToken().then(token => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch(err => {
+        res.status(400).send();
+    });
+});
+
 app.listen(process.env.PORT, () => {
     console.log(`Server started on port ${process.env.PORT}`);
 });
